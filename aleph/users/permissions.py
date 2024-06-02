1ea2f07346fd836842ac430f79b09150e39d0c5c
Talha-Ayub998 <talhaayub998@gmail.com>
2024-06-02 18:40:00 +0500

    Update the permission

--- aleph/users/permissions.py
+++ aleph/users/permissions.py
@@ -2,8 +2,8 @@ from rest_framework import permissions
 
 class IsAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.status == 'active'
+        return request.user and request.user.group == 'admin'
 
 class IsReviewerUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and not request.user.status == 'active'
+        return request.user and not request.user.group == 'admin'
