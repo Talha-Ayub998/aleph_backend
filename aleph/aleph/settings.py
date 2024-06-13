1ffcbd0a3f33b5df500e8b9e37fdd85d2bf2bbf3
Talha-Ayub998 <talhaayub998@gmail.com>
2024-06-13 22:41:55 +0500

    Add celery

--- aleph/aleph/settings.py
+++ aleph/aleph/settings.py
@@ -159,3 +159,11 @@ EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'talhaayub998@gmail.com'
 EMAIL_HOST_PASSWORD = 'fpxcnrzqbviplyfp'
+
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
