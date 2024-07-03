b6dd1fd3f6742b5143364b56a2e7f0acaca18b63
Talha-Ayub998 <talhaayub998@gmail.com>
2024-07-03 15:08:04 +0500

    Add error in status

--- aleph/users/tasks.py
+++ aleph/users/tasks.py
@@ -9,6 +9,9 @@ from users.models import *
 
 @shared_task
 def process_document(project_id, file_name, temp_file_path, bucket_name, unique_key):
+    status = {
+        'error': None
+    }
     try:
         project = Project.objects.get(id=project_id)
     except Project.DoesNotExist:
@@ -26,7 +29,6 @@ def process_document(project_id, file_name, temp_file_path, bucket_name, unique_
         return {'error': result['error']}
 
     # Calculate the checksum of the file
-    file_hash = calculate_checksum(temp_file_path, file_name)
     metadata = get_file_metadata(temp_file_path)
 
     # Upload to S3 using the unique key name
@@ -56,23 +58,26 @@ def process_document(project_id, file_name, temp_file_path, bucket_name, unique_
             text=result['text'],
             emails=emails
         )
-
-        pdf_document = fitz.open(temp_file_path)
-        for page_number in range(len(pdf_document)):
-            page = pdf_document.load_page(page_number)
-            pixmap = page.get_pixmap(matrix=fitz.Matrix(2, 2))
-            image_bytes = pixmap.tobytes()
-            s3_image_key = f"{unique_key}_page_{page_number + 1}.jpg"
-            if s3_service.upload_image_to_s3(image_bytes, bucket_name, s3_image_key):
-                image_url = s3_service.get_document_url(s3_file=s3_image_key, s3_bucket=bucket_name)
-                PageImage.objects.create(document=doc, page_number=page_number + 1, image_url=image_url)
-            pixmap = None  # Clean up the pixmap object
-        pdf_document.close()
+        try:
+            pdf_document = fitz.open(temp_file_path)
+            for page_number in range(len(pdf_document)):
+                page = pdf_document.load_page(page_number)
+                pixmap = page.get_pixmap(matrix=fitz.Matrix(2, 2))
+                image_bytes = pixmap.tobytes()
+                s3_image_key = f"{unique_key}_page_{page_number + 1}.jpg"
+                if s3_service.upload_image_to_s3(image_bytes, bucket_name, s3_image_key):
+                    image_url = s3_service.get_document_url(s3_file=s3_image_key, s3_bucket=bucket_name)
+                    PageImage.objects.create(document=doc, page_number=page_number + 1, image_url=image_url)
+                pixmap = None  # Clean up the pixmap object
+            pdf_document.close()
+        except Exception as e:
+            status['error'] = str(e)
+            print(f"Error processing PDF pages: {e}")
 
         if os.path.exists(temp_file_path):
             os.remove(temp_file_path)
 
-        return {'document_id': doc.id}
+        return {'document_id': doc.id, 'status':status}
     else:
         if os.path.exists(temp_file_path):
             os.remove(temp_file_path)
